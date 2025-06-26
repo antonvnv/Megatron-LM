8ae694fb0e3b1894958a7e8854be06f1dc690e3b
Anton Vorontsov <avorontsov@nvidia.com>
2025-06-25 18:38:42 -0700

    Megatron-LM: SamplingParams: Add token_callback

--- megatron/core/inference/text_generation_controllers/text_generation_controller.py
+++ megatron/core/inference/text_generation_controllers/text_generation_controller.py
@@ -809,6 +809,11 @@ class TextGenerationController:
                         log_probs, 2, indices
                     ).squeeze(2)
 
+                if sampling_params.token_callback:
+                    from megatron.core import parallel_state
+                    if parallel_state.is_pipeline_last_stage():
+                        sampling_params.token_callback(context_end_position)
+
                 context_start_position = context_end_position
 
                 if sampling_params.num_tokens_to_generate > 0:
